20cde4ad58a4e6b918d9d59425405bb677568097
Tretyakov Pavel <1@tretyakovpavel.ru>
2017-01-16 17:08:23 +0300

    Created course model

---
+++ src/app/courses/shared/course.model.ts
@@ -0,0 +1,6 @@
+export class Course {
+  name: string;
+  authors: string[];
+  duration: number;
+  created_at: Date;
+}
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,7 +1,17 @@
+import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
+
+import { AppState } from '../app.component';
+import { SET_TITLE } from '../app-title.reducer';
 
 @Component({
   styleUrls: ['./index.component.css'],
   templateUrl: './index.component.html',
 })
-export class IndexComponent {}
+export class IndexComponent {
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.store.dispatch({ type: SET_TITLE, payload: 'Main' });
+  }
+}
