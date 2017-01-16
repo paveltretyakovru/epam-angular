2ccca7f68dfd9f87ef17b6a01797c7525cb262a2
Tretyakov Pavel <1@tretyakovpavel.ru>
2017-01-16 15:06:51 +0300

    Add dispatch for show courses title

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,6 +1,16 @@
+import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
+
+import { AppState } from '../app.component';
+import { SET_TITLE } from '../app-title.reducer';
 
 @Component({
   template: `Courses component`,
 })
-export class CoursesComponent { }
+export class CoursesComponent {
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.store.dispatch({ type: SET_TITLE, payload: 'Courses' });
+  }
+}
